89fd1715914a1bbe1b705d0a2ecca5b0b61da2cd
istefanenko <stefanenko.irina@gmail.com>
2019-03-08 00:50:56 +0300

    home work lesson 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main (String[] arg) {
+        math();
+        equation();
+    }
+
+    private static void math(){
+        int a = 2;
+        int b = 5;
+        int sum = a + b;
+        System.out.println("Result a+b = " + sum);
+        int minus = a + b;
+        System.out.println("Result a-b = " + minus);
+        int multiplication = a * b;
+        System.out.println("Result a*b = " + multiplication);
+        int division = a / b;
+        System.out.println("Result a/b = " + division);
+        int divisionNod = a % b;
+        int divisionInt = a / b;
+        System.out.println("Result: " + divisionInt + " and remainder of the division " + divisionNod);
+        double d = 5.0;
+        double divisionDouble = a / d;
+        System.out.println("Result a/d = " + divisionDouble);
+    }
+
+    private static void equation(){
+        double a = 3.0;
+        double b = 45;
+        double x = (- b) / a;
+
+        System.out.println("Result equation 'ax + b = 0',  where x = " + x +
+                ". Check is " + (a * x + b) + " should be equаls 0." );
+
+
     }
 }
